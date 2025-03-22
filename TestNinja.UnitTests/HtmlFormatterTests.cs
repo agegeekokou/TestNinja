0a850b24dd984174e861fe442d106182bba29712
Kokou AGEGEE <delaliage@yahoo.fr>
2025-03-22 10:34:37 +0100

    Add Reservation, Math and HtmlFormatter Tests

---
+++ TestNinja.UnitTests/HtmlFormatterTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class HtmlFormatterTests
+    {
+        [Test]
+        public void FormatAsBold_WhenCalled_ShouldEncloseTheStringWithStrongElement()
+        {
+            var formater = new HtmlFormatter();
+
+            var result = formater.FormatAsBold("abc");
+
+            Assert.That(result, Is.EqualTo("<strong>abc</strong>"));
+        }
+    }
+}
