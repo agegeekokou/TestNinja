a117fad899ca356860f1a363a3090e7c0acc2141
Kokou AGEGEE <delaliage@yahoo.fr>
2025-04-18 07:37:12 +0200

    Add VideoServiceTests

--- TestNinja/Mocking/FileReader.cs
+++ TestNinja/Mocking/FileReader.cs
@@ -2,11 +2,6 @@
 
 namespace TestNinja.Mocking
 {
-    public interface IFileReader
-    {
-        string Read(string path);
-    }
-
     public class FileReader : IFileReader
     {
         public string Read(string path)
