5e1b981322c3006340e0415bfcb3e1787fc34bd7
Kokou AGEGEE <delaliage@yahoo.fr>
2025-05-17 20:18:57 +0200

    Test EmployeeController and InstallerHelper

--- TestNinja/Mocking/FileDownloader.cs
+++ TestNinja/Mocking/FileDownloader.cs
@@ -1,18 +1,19 @@
-﻿using System.Net;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace TestNinja.Mocking
 {
-    public interface IFileDownloader
-    {
-        void DownloadFile(string url, string path);
-    }
-
     public class FileDownloader : IFileDownloader
     {
         public void DownloadFile(string url, string path)
         {
             var client = new WebClient();
-            client.DownloadFile(url, path);            
+
+            client.DownloadFile(url, path);
         }
     }
 }
